0bc05d231ed92ba3a86601e07ff47da9e0617ccc
yoda-ocu <m_yodai_apple@hotmail.com>
2020-02-08 15:52:06 +0900

    Update: sim_robot_run.launch
    敵機体をキーボードでコントロールできるように設定。
    起動用のスクリプトとLaunchファイルの作成。
    (bash script/start.sh -> bash script/toVerify.sh)

---
+++ scripts/toVerify.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# set judge server state "running"
+bash judge/test_scripts/set_running.sh localhost:5000
+gnome-terminal -e "roslaunch burger_war enemy_teleop.launch"
+# launch robot control node
+roslaunch burger_war sim_robot_run_2.launch
+
