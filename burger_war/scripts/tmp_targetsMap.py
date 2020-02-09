87299ca68643b8e9a42fc7e8c618f0ac332ef3f2
yoda-ocu <m_yodai_apple@hotmail.com>
2020-02-09 22:45:42 +0900

    Update: tmp_myState_listener.py
    とりあえず最も近いターゲットまで自動移動する部分を追加。ただし、うまく行かないことも多い。
    
    TO-DO:
    - 最初の方の起動するまでのwait
    - 経路が作れなかった時の例外処理
    - 目標位置の距離(distance)の調整
    - (目標位置に達した」の判断

--- burger_war/scripts/tmp_targetsMap.py
+++ burger_war/scripts/tmp_targetsMap.py
@@ -1,3 +1,8 @@
+import math
+
+import rospy
+from geometry_msgs.msg import PoseStamped
+
 def getTargetsMapOnGAZEBO():
     Targets = {
         "FriedShrimp_N":    [0.0,       +0.35/2.0],
@@ -31,6 +36,42 @@ def getTargetsMap():
         "Pudding_S":        [-0.53-0.15/2.0,    +0.53],        
     }
     return Targets
+
+distance = 0.25  # magic parm of distance to the target
+
+def getGoal(targetPos, targetName):
+    goalPos = targetPos
+    goalDire = [0.0,0.0,0.0,1.0]
+    
+    if targetName[-1] == "N":
+        goalPos[0] = goalPos[0] + distance
+        goalDire[2] = 1.0
+        goalDire[3] = 0.0
+    elif targetName[-1] == "S":
+        goalPos[0] = goalPos[0] - distance
+        goalDire[2] = 0.0
+        goalDire[3] = 1.0
+    elif targetName[-1] == "W":
+        goalPos[1] = goalPos[1] + distance
+        goalDire[2] = math.sin(-math.pi/4.0)
+        goalDire[3] = math.cos(-math.pi/4.0)
+    elif targetName[-1] == "E":
+        goalPos[1] = goalPos[1] - distance
+        goalDire[2] = math.sin(math.pi/4.0)
+        goalDire[3] = math.cos(math.pi/4.0)
+    elif targetName == "enemy":
+        print("Target is ENEMY!!")        
+    else:
+        print("ERROR!!!")
+
+    target_pos = PoseStamped()
+    target_pos.header.frame_id = "map"
+    target_pos.pose.position.x = goalPos[0]
+    target_pos.pose.position.y = goalPos[1]
+    target_pos.pose.orientation.z = goalDire[2]
+    target_pos.pose.orientation.w = goalDire[3]
+
+    return target_pos
 
 def getNearestTarget(Targets, x, y):
     dist = 99.0    
--- scripts/toVerify.sh
+++ scripts/toVerify.sh
@@ -2,7 +2,9 @@
 
 # set judge server state "running"
 bash judge/test_scripts/set_running.sh localhost:5000
-gnome-terminal -e "roslaunch burger_war enemy_teleop.launch"
+# gnome-terminal -e "roslaunch burger_war burger_teleop.launch"
+# gnome-terminal -e "roslaunch burger_war enemy_teleop.launch"
+
 # launch robot control node
 roslaunch burger_war sim_robot_run_2.launch
 
