a70b94de3a5c9ab299e53805c88d6295094032a4
yoda-ocu <m_yodai_apple@hotmail.com>
2020-02-29 12:44:12 +0900

    Update: tmp_myState_listenr.py
    現在の目標位置をRviz上に表示するように変更

--- burger_war/scripts/tmp_myState_listener.py
+++ burger_war/scripts/tmp_myState_listener.py
@@ -9,12 +9,14 @@ import tmp_targetsMap
 from copy import deepcopy as dcopy
 from geometry_msgs.msg import PoseStamped
 from burger_war.msg import war_state
+from jsk_rviz_plugins.msg import OverlayText
 
 class MyStateBot(object):
     def __init__(self, mySide = 'r'):
         self.mySide = mySide
         self.map = tmp_targetsMap.getTargetsMap()        
         self.goalPub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=5)                
+        self.myStatePub = rospy.Publisher('my_state',OverlayText,queue_size=5)
         self.warStateSub = rospy.Subscriber('/war_state', war_state, self.warStateCallback)
         
         # self.cnt = 10
@@ -23,6 +25,9 @@ class MyStateBot(object):
         self.war_state.target_owner = []
         self.war_state.target_point = []
         
+        self.my_state_text = OverlayText()
+        self.my_state_text.text = ""
+
     def strategy(self):     
         
         r = rospy.Rate(10.0)
@@ -34,12 +39,18 @@ class MyStateBot(object):
                 self.pose_x = trans[0]
                 self.pose_y = trans[1]                
                 nearestTargetName = tmp_targetsMap.getNearestTarget(dcopy(self.map),dcopy(self.pose_x),dcopy(self.pose_y),self.war_state)
-                nearestTargetPos = dcopy(self.map[nearestTargetName])
-                
-                target_pos = tmp_targetsMap.getGoal(nearestTargetPos, nearestTargetName)                        
-                target_pos.header.stamp = rospy.Time.now()                                                               
-                self.goalPub.publish(target_pos)                                              
 
+                if nearestTargetName == "":
+                    # target_pos = t
+                    self.my_state_text.text = "All Targets are MINE !!!!!"
+                else:
+                    self.my_state_text.text = "Current Target : " + nearestTargetName
+                    
+                    nearestTargetPos = dcopy(self.map[nearestTargetName])                                    
+                    target_pos = tmp_targetsMap.getGoal(nearestTargetPos, nearestTargetName)                        
+                    target_pos.header.stamp = rospy.Time.now()                                                               
+                self.goalPub.publish(target_pos)                                             
+                self.myStatePub.publish(self.my_state_text)
             except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
                 rospy.logerr("Failed to gettransform")
                 continue            
